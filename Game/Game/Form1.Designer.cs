6d7097e19520d276ec7950fdcd10715198eefc89
chegounian <alititan20ch@gmail.com>
2024-05-10 13:23:46 +0330

    Add files via upload

---
+++ Game/Game/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel9 = new System.Windows.Forms.Panel();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.panel10 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LightGray;
+            this.panel1.Location = new System.Drawing.Point(107, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(253, 10);
+            this.panel1.TabIndex = 1;
+            this.panel1.MouseLeave += new System.EventHandler(this.panel5_MouseLeave);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.LightGray;
+            this.panel2.Location = new System.Drawing.Point(107, 39);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(216, 12);
+            this.panel2.TabIndex = 1;
+            this.panel2.MouseLeave += new System.EventHandler(this.panel5_MouseLeave);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.LightGray;
+            this.panel3.Location = new System.Drawing.Point(347, 20);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(13, 341);
+            this.panel3.TabIndex = 1;
+            this.panel3.MouseLeave += new System.EventHandler(this.panel5_MouseLeave);
+            // 
+            // panel7
+            // 
+            this.panel7.BackColor = System.Drawing.Color.LightGray;
+            this.panel7.Location = new System.Drawing.Point(313, 39);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(13, 274);
+            this.panel7.TabIndex = 1;
+            this.panel7.MouseLeave += new System.EventHandler(this.panel5_MouseLeave);
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.Color.LightGray;
+            this.panel6.Location = new System.Drawing.Point(160, 350);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(200, 14);
+            this.panel6.TabIndex = 1;
+            this.panel6.MouseLeave += new System.EventHandler(this.panel5_MouseLeave);
+            // 
+            // panel9
+            // 
+            this.panel9.BackColor = System.Drawing.Color.LightGray;
+            this.panel9.Location = new System.Drawing.Point(126, 302);
+            this.panel9.Name = "panel9";
+            this.panel9.Size = new System.Drawing.Size(200, 14);
+            this.panel9.TabIndex = 1;
+            this.panel9.MouseLeave += new System.EventHandler(this.panel5_MouseLeave);
+            // 
+            // panel8
+            // 
+            this.panel8.BackColor = System.Drawing.Color.LightGray;
+            this.panel8.Location = new System.Drawing.Point(126, 302);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(18, 230);
+            this.panel8.TabIndex = 1;
+            this.panel8.MouseLeave += new System.EventHandler(this.panel5_MouseLeave);
+            // 
+            // panel10
+            // 
+            this.panel10.BackColor = System.Drawing.Color.LightGray;
+            this.panel10.Location = new System.Drawing.Point(160, 364);
+            this.panel10.Name = "panel10";
+            this.panel10.Size = new System.Drawing.Size(13, 148);
+            this.panel10.TabIndex = 1;
+            this.panel10.MouseLeave += new System.EventHandler(this.panel5_MouseLeave);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.SeaShell;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(60, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "start";
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.LightGray;
+            this.panel4.Location = new System.Drawing.Point(126, 518);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(196, 14);
+            this.panel4.TabIndex = 1;
+            this.panel4.MouseLeave += new System.EventHandler(this.panel5_MouseLeave);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Game.Properties.Resources.Screenshot__1_;
+            this.pictureBox1.Location = new System.Drawing.Point(107, 21);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(19, 19);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseEnter += new System.EventHandler(this.pictureBox1_MouseEnter);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(802, 565);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel10);
+            this.Controls.Add(this.panel8);
+            this.Controls.Add(this.panel7);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel6);
+            this.Controls.Add(this.panel9);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel9;
+        private System.Windows.Forms.Panel panel8;
+        private System.Windows.Forms.Panel panel10;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Game/Game/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void textBox3_MouseLeave(object sender, EventArgs e)
+        {
+        }
+
+        private void textBox3_MouseLeave_1(object sender, EventArgs e)
+        {
+        }
+
+        private void panel5_MouseLeave(object sender, EventArgs e)
+        {
+            MessageBox.Show("game over");
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Left += 5;
+
+        }
+
+        private void pictureBox1_MouseEnter(object sender, EventArgs e)
+        {
+            MessageBox.Show("You Win");
+        }
+    }
+}
